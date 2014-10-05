63311795834a5c6d01f8816488df702d88ed367e
Vassilis Harisopoulos <the.machine.headed@gmail.com>
2014-10-05 18:55:28 +0300

    tail recursiveness fixed

--- words.pl
+++ words.pl
@@ -17,17 +17,17 @@
 
 parseSentence(L, Pd) :- parseSentence(L, [], Aux), reverse(Aux, Pd).
 
-parseSentence([], Acc, Acc).
 parseSentence(L, Acc, Tr) :- makeword(L, W, Rest), parseSentence(Rest, [(W, word)|Acc], Tr).
 parseSentence(L, Acc, Tr) :- makerest(L, R, Rest), parseSentence(Rest, [(R, rest)|Acc], Tr).
+parseSentence([], Acc, Acc).
 
 makeword(L, W, Rest) :- L = [H|_], char_type(H, alpha), wlist(L, [], Aux, Rest), reverse(Aux, W).
 makerest(L, R, Rest) :- L = [H|_], \+ char_type(H, alpha), rlist(L, [], Aux, Rest), reverse(Aux, R).
 
-rlist([], Acc, Acc, []).
 rlist([H|T], Acc, Acc, [H|T]) :- char_type(H, alpha).
 rlist([H|T], Acc, R, Rest) :- \+ char_type(H, alpha), rlist(T, [H|Acc], R, Rest).
+rlist([], Acc, Acc, []).
 
-wlist([], Acc, Acc, []).
 wlist([H|T], Acc, Acc, [H|T]) :- \+ char_type(H, alpha).
 wlist([H|T], Acc, R, Rest) :- char_type(H, alpha), wlist(T, [H|Acc], R, Rest).
+wlist([], Acc, Acc, []).
