15c309b94053f3703a26d3c0c218ec17beeb7a64
VHarisop <the.machine.headed@gmail.com>
2015-01-15 03:00:13 +0200

    added pyramid plotting

---
+++ pyramid.pl
@@ -0,0 +1,10 @@
+pyramid(N) :- pyramid(N, N-1).
+
+pyramid(0, _) :- nl, !.
+pyramid(N, K) :- N1 is N-1,
+				 foreach(between(1, N1, _), write(' ')),
+				 Q is 2 * (K - N1) + 1,
+				 foreach(between(1, Q, _), write('*')),
+				 foreach(between(1, N1, _), write(' ')), 
+				 nl, pyramid(N1, K).
+
